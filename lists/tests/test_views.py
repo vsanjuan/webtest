ed5cb5df46f557a038d6e8451698d0f78c475392
vsanjuan <salvador.sanjuan@gmail.com>
2014-10-24 20:11:18 +0200

    Refactor list view to handle new item POSTs

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -45,6 +45,36 @@ class ListViewTest(TestCase):
     self.assertNotContains(response, 'other list item 1' )
     self.assertNotContains(response, 'other list item 2' )
 
+  def test_passes_correct_list_to_template(self):
+    other_list = List.objects.create()
+    correct_list = List.objects.create()
+    response = self.client.get('/lists/%d/' % (correct_list.id,))
+    self.assertEqual(response.context['list'], correct_list)
+
+  def test_can_save_a_POST_request_to_an_existing_list(self):
+    other_list = List.objects.create()
+    correct_list = List.objects.create()
+
+    self.client.post(
+        '/lists/%d/' % (correct_list.id,),
+        data={'new_item': 'A new item for an existing list'}
+        )
+    self.assertEqual(Item.objects.count(), 1)
+    new_item = Item.objects.first()
+    self.assertEqual(new_item.text, 'A new item for an existing list')
+    self.assertEqual(new_item.list, correct_list)
+
+  def test_POST_redirects_to_list_view(self):
+    other_list = List.objects.create()
+    correct_list = List.objects.create()
+
+    response = self.client.post(
+        '/lists/%d/' % (correct_list.id,),
+        data= {'new_item':'A new item for an existing list'}
+        )
+    self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+
+
 class NewListTest(TestCase):
 
   def test_saving_a_POST_request(self):
@@ -66,11 +96,6 @@ class NewListTest(TestCase):
     new_list = List.objects.first()
     self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
 
-  def test_passes_correct_list_to_template(self):
-    other_list = List.objects.create()
-    correct_list = List.objects.create()
-    response = self.client.get('/lists/%d/' % (correct_list.id,))
-    self.assertEqual(response.context['list'], correct_list)
 
   def test_validation_errors_are_sent_back_to_home_page_template(self):
     response = self.client.post('/lists/new', data={'new_item':''})
@@ -85,30 +110,30 @@ class NewListTest(TestCase):
     self.assertEqual(List.objects.count(), 0)
     self.assertEqual(Item.objects.count(), 0)
 
-class NewItemTest(TestCase):
+# class NewItemTest(TestCase):
 
-  def test_can_save_a_POST_request_to_an_existing_list(self):
-    other_list = List.objects.create()
-    correct_list = List.objects.create()
+#   def test_can_save_a_POST_request_to_an_existing_list(self):
+#     other_list = List.objects.create()
+#     correct_list = List.objects.create()
 
-    self.client.post(
-        '/lists/%d/add_item' % (correct_list.id,),
-        data={'new_item' : 'A new item for an existing list'}
-    )
+#     self.client.post(
+#         '/lists/%d/add_item' % (correct_list.id,),
+#         data={'new_item' : 'A new item for an existing list'}
+#     )
 
-    self.assertEqual(Item.objects.count(), 1)
-    new_item = Item.objects.first()
-    self.assertEqual(new_item.text, 'A new item for an existing list')
-    self.assertEqual(new_item.list, correct_list)
+#     self.assertEqual(Item.objects.count(), 1)
+#     new_item = Item.objects.first()
+#     self.assertEqual(new_item.text, 'A new item for an existing list')
+#     self.assertEqual(new_item.list, correct_list)
 
-  def test_redirects_to_list_view(self):
-    other_list = List.objects.create()
-    correct_list = List.objects.create()
+#   def test_redirects_to_list_view(self):
+#     other_list = List.objects.create()
+#     correct_list = List.objects.create()
 
-    response = self.client.post(
-          '/lists/%d/add_item' % (correct_list.id,),
-          data={'new_item': 'A new item for an existing list'}
-    )
+#     response = self.client.post(
+#           '/lists/%d/add_item' % (correct_list.id,),
+#           data={'new_item': 'A new item for an existing list'}
+#     )
 
-    self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+#     self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
 
