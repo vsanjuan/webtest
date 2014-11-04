61a08b4919741fcf6378b01adfe7171c594013b9
vsanjuan <salvador.sanjuan@gmail.com>
2014-11-04 09:41:40 +0100

    Validation error is passed to the form

--- lists/forms.py
+++ lists/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from lists.models import Item
 
 EMPTY_LIST_ERROR = "You can't have an empty list item"
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
 class ItemForm(forms.models.ModelForm):
 
@@ -19,8 +20,38 @@ class ItemForm(forms.models.ModelForm):
 		}
 
 	def save(self, for_list):
-		self.instance.list = for_list
-		return super().save()
+		self.instance.list = for_list		# 
+		return super().save()				# Saves the instance with the save parent's method
+
+class ExistingListItemForm(ItemForm):
+
+	def __init__(self, for_list, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+
+	def validate_unique(self):
+		try:
+			self.instance.validate_unique()
+		except ValidationError as e:
+			e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
+			self._update_errors(e)
+
+	# class Meta:
+	# 	model = Item
+	# 	fields = ('text',)
+	# 	widgets= {
+	# 		'text': forms.fields.TextInput(attrs={
+	# 		'placeholder': 'Enter a to-do item',
+	# 		'class': 'form-control input-lg',
+	# 		}),
+	# 	}
+	# 	error_messages = {
+	# 		'text': {'required': EMPTY_LIST_ERROR}
+	# 	}
+
+	# def save(self, for_list):
+	# 	self.instance.list = for_list
+	# 	return super().save()
+
 
 		
 # class ItemForm(forms.Form):
